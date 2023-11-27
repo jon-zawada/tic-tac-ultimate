88fe9060e19b8f19d0da83309aa24b4c45a24b0c
jon-zawada <jonathan_zawada@yahoo.com>
2023-11-26 21:58:35 -0800

    css revamp for responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 import Miniboard from "./Miniboard";
+import { Container, Col, Button } from "react-bootstrap";
 import { checkCats, checkWinner } from "./utils";
 
 const initState = [
@@ -69,7 +70,7 @@ function App() {
   const [playableBox, setPlayableBox] = React.useState<any[]>([]);
   const [currentPlay, setCurrentPlay] = React.useState("X");
 
-  const updatePlayable = (y:number, z:number) => {
+  const updatePlayable = (y: number, z: number) => {
     if (boxStatus[y][z] !== "") {
       setPlayableBox([]);
     } else {
@@ -78,7 +79,7 @@ function App() {
   };
 
   const play = (i: number, j: number, y: number, z: number) => {
-    if(state[i][j][y][z] === "") {
+    if (state[i][j][y][z] === "") {
       setState((prev) => {
         prev[i][j][y][z] = currentPlay;
         updatePlayable(y, z);
@@ -87,45 +88,59 @@ function App() {
           setBoxStatus((prev) => {
             prev[i][j] = currentPlay;
             const gameOver = checkWinner(prev) || checkCats(prev);
-            if(gameOver) {
-              alert("GAME WON REFRESH TO PLAY AGAIN")
+            if (gameOver) {
+              alert("GAME WON REFRESH TO PLAY AGAIN");
             }
             return prev;
           });
         }
-        if(checkCats(prev[i][j])) {
+        if (checkCats(prev[i][j])) {
           setPlayableBox([]);
-          setBoxStatus(prev => {
+          setBoxStatus((prev) => {
             prev[i][j] = "CATS";
             return prev;
-          })
+          });
         }
         return prev;
       });
       setCurrentPlay(currentPlay === "X" ? "O" : "X");
-    } 
+    }
+  };
+
+  const resetGame = () => {
+    setState(initState); //this doesnt work
+    setBoxStatus(initBoxStatus);
+    setPlayableBox([]);
+    setCurrentPlay("X");
   };
 
   return (
     <>
-    <div className="app-wrapper">
-      <div className="outer-grid-wrapper">
-        {state.map((row, i) =>
-          row.map((grid, j) => (
-            <div key={`box-${i}${j}`}>
-              <Miniboard
-                playableBox={playableBox}
-                status={boxStatus[i][j]}
-                grid={grid}
-                i={i}
-                j={j}
-                play={play}
-              />
-            </div>
-          ))
-        )}
+      <div className="header">
+        <h1>TIC TAC TOE</h1>
+        <h2>Ultimate</h2>
       </div>
-    </div>
+      <Container fluid className="app-wrapper">
+        <Container className="outer-grid-wrapper">
+          {state.map((row, i) =>
+            row.map((grid, j) => (
+              <Col key={`box-${i}${j}`}>
+                <Miniboard
+                  playableBox={playableBox}
+                  status={boxStatus[i][j]}
+                  grid={grid}
+                  i={i}
+                  j={j}
+                  play={play}
+                />
+              </Col>
+            ))
+          )}
+        </Container>
+        {/* <Button variant="primary" size="lg" onClick={resetGame}>
+          Restart game
+        </Button> */}
+      </Container>
     </>
   );
 }
